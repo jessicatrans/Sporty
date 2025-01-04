92d21b0636d12785d8563cc175a8b5ee41195acc
jessicatrans <jesstrans02@gmail.com>
2025-01-03 18:06:31 -0800

    Initial Commit

---
+++ Sporty/SportyApp.swift
@@ -0,0 +1,32 @@
+//
+//  SportyApp.swift
+//  Sporty
+//
+//  Created by Jessica Trans on 1/3/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SportyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SportyTests/SportyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SportyTests.swift
+//  SportyTests
+//
+//  Created by Jessica Trans on 1/3/25.
+//
+
+import Testing
+@testable import Sporty
+
+struct SportyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
